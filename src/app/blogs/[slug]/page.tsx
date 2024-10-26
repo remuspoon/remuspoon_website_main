3e3e251e1733ccac73dcfabcfab35b1792704541
Remus Desktop <remuspoon@gmail.com>
2024-10-26 18:14:10 +0100

    Blog article page

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,7 +1,37 @@
-export default function BlogPage({params}: {params: {slug: string}}) {
+import { client } from '@/sanity/lib/client'
+import { PortableText } from '@portabletext/react'
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
+
+export interface BlogPageType {
+    currentSlug: string;
+    title: string;
+    publishedAt: string;
+    body: any;
+    mainImage: any;
+}
+
+async function getData(slug: string) {
+    const query = `*[_type == "post" && slug.current == '${slug}']{
+        "currentSlug": slug.current,
+        title,
+        publishedAt,
+        body,
+        mainImage
+    }[0]`
+    const data = await client.fetch(query)
+    return data
+}
+
+export default async function BlogPage({params}: {params: {slug: string}}) {
+    const data: BlogPageType = await getData(params.slug)
     return (
-        <div>
-            <h1 className="text-light">{params.slug}</h1>
+        <div className='bg-light pt-20'>
+            <div className='prose prose-violet font-cabin '>
+                {data.title}
+                <PortableText value={data.body}/>
+                <Image src={urlFor(data.mainImage).url()} className='w-full h-full object-cover' alt={data.title} width={500} height={500}/>
+            </div>
         </div>
     )
 }
